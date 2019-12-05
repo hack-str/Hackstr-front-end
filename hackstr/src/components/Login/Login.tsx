0c78ed5eacf3fdbacb2e10122a8d9b0a7cee7037
tachester <Trevin.Chester@gmail.com>
2019-12-05 15:30:54 -0500

    navbar up

--- hackstr/src/components/Login/Login.tsx
+++ hackstr/src/components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Login.css';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
     state = {
@@ -13,7 +14,7 @@ class Login extends Component {
                     <div className="row">
                         <div className="col-md-4 col-sm-4 col-xs-12"></div>
                         <div className="col-md-4 col-sm-4 col-xs-12">
-                            <form className="form-container backgroundcolor">
+                            <form  className="form-container backgroundcolor">
                                 <div className="form-group">
                                     <h2>User Login</h2>
                                     <label >Email address</label> <input
@@ -25,15 +26,27 @@ class Login extends Component {
                                         type="password" className="form-control" id="exampleInputPassword1"
                                         placeholder="Password"></input>
                                 </div>
-                                    <button type="submit" className="btn btn-info btn-block">Submit</button>
-				</form>
-			</div>
-                            <div className="col-md-4 col-sm-4 col-xs-12"></div>
+                            </form>
+                            
+                        <Link to="">
+                                <button type="submit" className="btn btn-info btn-block">Submit</button>
+                            </Link>
                         </div>
-                    </div>>
+                        <div className="col-md-4 col-sm-4 col-xs-12"></div>
+                    </div>
+                </div>>
       </div>
-                );
-            }
-        }
+        );
+    }
+
+    hackySubmit = (event: any) => {
+        if (event.preventDefault)
+            event.preventDefault();
+        else
+            event.returnValue = false;
+
         
-        export default Login;
+    }
+}
+
+export default Login;
