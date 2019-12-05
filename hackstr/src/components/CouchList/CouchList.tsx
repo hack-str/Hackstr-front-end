120473f290f465dbedccc60a9eb5a9a702873b2b
tachester <Trevin.Chester@gmail.com>
2019-12-05 13:38:35 -0500

    Filter menu readded

--- hackstr/src/components/CouchList/CouchList.tsx
+++ hackstr/src/components/CouchList/CouchList.tsx
@@ -5,80 +5,82 @@ import "./CouchList.css";
 import MicroCouch from "./MicroCouch";
 import { getListings } from "../../http/Listing";
 
-interface Istate{
-    microcouches:any[];
-    inputField:string
+interface Istate {
+    microcouches: any[];
+    inputField: string
 }
 class CouchList extends Component {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      microcouches: [{address:"Robert C"}, {address:"The Steve"}, {address:"Carolyn"}, {address:"Ravi"}],
-      inputField: ""
-    };
-  }
-  async componentDidMount() {
-    const listing = await getListings();
-    console.log(listing);
-    listing!==[] ? this.setState({ microcouches: listing }) : console.log(false);
-  }
-  state:Istate;
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            microcouches: [{ address: "Robert C" }, { address: "The Steve" }, { address: "Carolyn" }, { address: "Ravi" }],
+            inputField: ""
+        };
+    }
+    async componentDidMount() {
+        const listing = await getListings();
+        console.log(listing);
+        listing !== [] ? this.setState({ microcouches: listing }) : console.log(false);
+    }
+    state: Istate;
 
-  render() {
-    return (
-      <div className="CouchList">
-        <table className="table curveEdge bg hidden">
-          <thead>
-            <tr id="customHeaderRow curveEdge">
-              <th scope="col">
-                <h2>Couch Listings</h2>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.microcouches.map((villain,index) => {
-              return (
-                <tr className="customRows" key={index}>
-                  <td>
-                    <MicroCouch>{villain.address}</MicroCouch>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-
-        {/* <form onSubmit={this.addVillain}>
-                    <div className="form-group">
-                        <label >Add a villain</label>
-                        <input value= {this.getInputField()} onChange= {this.inputFieldChanged} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+    render() {
+        return (
+            <div className="CouchList">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="filterSection curveEdge hidden">
+                            <h2> working</h2>
+                        </div>
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form> */}
-      </div>
-    );
-  }
-
-  /* getInputField = () =>{
-        return this.state.inputField;
+                    <div className="col-9">
+                        <table className="table curveEdge bg hidden">
+                            <thead>
+                                <tr id="customHeaderRow curveEdge">
+                                    <th scope="col">
+                                        <h2>Couch Listings</h2>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.microcouches.map((villain, index) => {
+                                    return (
+                                        <tr className="customRows" key={index}>
+                                            <td>
+                                                <Link to="/couch/:id">
+                                                    <MicroCouch>{villain.address}</MicroCouch>
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
     }
 
-    inputFieldChanged = (event) =>{
-        this.setState({
-            inputField: event.target.value
-          });
-    }
-
-    addVillain = (event) => {
-        if (event.preventDefault)
-            event.preventDefault();
-        else
-            event.returnValue = false;
-    
-        this.state.bounties.push(this.state.inputField);
-        this.setState({bounties: this.state.bounties});
-    } */
+    /* getInputField = () =>{
+          return this.state.inputField;
+      }
+  
+      inputFieldChanged = (event) =>{
+          this.setState({
+              inputField: event.target.value
+            });
+      }
+  
+      addVillain = (event) => {
+          if (event.preventDefault)
+              event.preventDefault();
+          else
+              event.returnValue = false;
+      
+          this.state.bounties.push(this.state.inputField);
+          this.setState({bounties: this.state.bounties});
+      } */
 }
 
 export default CouchList;
