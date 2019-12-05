61e5c013dfeed735350b86bd584e4208c1d2a0b1
harveyhill <43756739+harveyhill@users.noreply.github.com>
2019-12-05 11:35:51 -0500

    updates to couch

--- hackstr/src/components/Couch/Couch.tsx
+++ hackstr/src/components/Couch/Couch.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Couch.css';
+
 
 let couchListing = {
     id: '',
@@ -12,13 +14,14 @@ let couchListing = {
 }
 
 const Couch = (props: any) => {
-    return <section>
+    return <section id="CouchSection" >
         <img id="CouchImage" src="{couchListing.pictureUrl}" />
-        <div><span>Listing Name: </span>{couchListing.id}</div>
-        <div><span>Host: </span>{couchListing.userId}</div>
-        <div><span>Address: </span>{couchListing.address}</div>
-        <div><span>Rental Price: </span>{couchListing.rentPrice}</div>
-        <div><span>Description: </span>{couchListing.description}</div>
+        <div><span className="Label">Listing Name: </span>{couchListing.id}</div>
+        <div><span className="Label">Host: </span>{couchListing.userId}</div>
+        <div><span className="Label">Address: </span>{couchListing.address}</div>
+        <div><span className="Label">Rental Price: </span>{couchListing.rentPrice}</div>
+        <div><span className="Label">Description: </span>{couchListing.description}</div>
+        <div><button id="CouchButton" type="button">Contact Host</button></div>
     </section>
 }
 
