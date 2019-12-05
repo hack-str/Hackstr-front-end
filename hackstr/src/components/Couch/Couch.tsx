d6c54a14427d6b5e36bd74583b04d0c25ab73bb3
harveyhill <43756739+harveyhill@users.noreply.github.com>
2019-12-05 11:50:41 -0500

    Updates to couch

--- hackstr/src/components/Couch/Couch.tsx
+++ hackstr/src/components/Couch/Couch.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Couch.css';
+import { Container, Col, Row } from 'reactstrap';
 
 
 let couchListing = {
@@ -15,13 +16,19 @@ let couchListing = {
 
 const Couch = (props: any) => {
     return <section id="CouchSection" >
-        <img id="CouchImage" src="{couchListing.pictureUrl}" />
-        <div><span className="Label">Listing Name: </span>{couchListing.id}</div>
+        <Container>
+        <Row>
+        <Col><img id="CouchImage" src="{couchListing.pictureUrl}" /></Col>
+        <Col><span className="Label">Listing Name: </span>{couchListing.id}</Col>
+        </Row>
+        <Row>
         <div><span className="Label">Host: </span>{couchListing.userId}</div>
         <div><span className="Label">Address: </span>{couchListing.address}</div>
+        </Row>
         <div><span className="Label">Rental Price: </span>{couchListing.rentPrice}</div>
         <div><span className="Label">Description: </span>{couchListing.description}</div>
-        <div><button id="CouchButton" type="button">Contact Host</button></div>
+        <div><button id="CouchButton" color="Primary">Contact Host</button></div>
+        </Container>
     </section>
 }
 
