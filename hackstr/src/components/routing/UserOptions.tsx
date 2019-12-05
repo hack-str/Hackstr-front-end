39a8090863fe78f8158082d9c3297c3a627f0081
tachester <Trevin.Chester@gmail.com>
2019-12-05 15:18:06 -0500

    navbar up

--- hackstr/src/components/routing/UserOptions.tsx
+++ hackstr/src/components/routing/UserOptions.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-export const UserOptions:React.FC<any>=(props:any)=>{
-    return (
-        <>
-        <Dropdown isOpen={props.dropdownOpen} toggle={props.toggle}>
+export const UserOptions: React.FC<any> = (props: any) => {
+  return (
+    <>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col">
+          <Link to="/createCouch">
+            <a className="nav-link" href="#">Create Couch</a>
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/login">
+            <a className="nav-link" href="#">Login</a>
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/login">
+            <a className="nav-link" href="#">Logout</a>
+          </Link>
+        </div>
+      </div>
+      {/* <Dropdown isOpen={props.dropdownOpen} toggle={props.toggle}>
           <DropdownToggle caret>{props.username}</DropdownToggle>
           <DropdownMenu>
             <DropdownItem>Create Listing</DropdownItem>
             <DropdownItem>divider</DropdownItem>
             <DropdownItem>Sign Out</DropdownItem>
           </DropdownMenu>
-        </Dropdown>
-      
-      </>
-    )
+        </Dropdown> */}
+
+    </>
+  )
 }
 export default UserOptions;
