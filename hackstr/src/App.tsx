d6e3aa190cdbf56f10a57e58af95bb15f27206ab
TChester <Trevin.Chester@gmail.com>
2019-12-05 12:12:32 -0500

    couchlist stylings

--- hackstr/src/App.tsx
+++ hackstr/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
       <Switch>
         {/* <Route path='/login' component={Login}/>
         <Route path='createCouch' component={CreateCouch}/> */}
-        <Route path= '/couch/:id' component={Couch}/>
+        {/* <Route path= '/couch/:id' component={Couch}/> */}
         <Route component={CouchList}/>
       </Switch>
     </div>
